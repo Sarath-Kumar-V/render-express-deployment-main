d95205e6927b2b923037e0eb3ce0317729dede8d
Sarath-Kumar-V <sarathkumaran07@gmail.com>
2026-02-15 15:01:40 +0530

    done!!2

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: ['https://stellular-pie-237548.netlify.app','https://reliable-concha-02ceae.netlify.app/login','http://localhost:5173'],
+  origin: ['https://stellular-pie-237548.netlify.app','https://reliable-concha-02ceae.netlify.app','http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
